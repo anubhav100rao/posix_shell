0e734083efe2489ff5f582b40419333b8e458d3d
Anubhav Kumar Rao <anubhav100rao@Anubhavs-MacBook-Air.local>
2025-01-30 23:11:23 +0530

    stage 4

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,14 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func handleInvalidCommand(invalid_command string) {
 	fmt.Printf("%s: command not found\n", invalid_command)
 }
 
-func main() {
+func handleExit(args []string) {
+	if len(args) < 2 {
+		log.Fatal("exit: too less arguments")
+	}
+	status, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Fatal("exit: invalid status code")
+	}
+	os.Exit(status)
+}
 
+func handleCommand(command string) {
+	args := strings.Split(command, " ")
+	switch args[0] {
+	case "exit":
+		handleExit(args)
+	default:
+		handleInvalidCommand(command)
+	}
+}
+
+func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -22,6 +44,6 @@ func main() {
 			log.Fatal(err)
 		}
 		input = input[:len(input)-1]
-		handleInvalidCommand(input)
+		handleCommand(input)
 	}
 }
